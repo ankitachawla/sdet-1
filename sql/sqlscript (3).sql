f3db16f565a200f86812baade7a58ad1621474ca
Ankichaw <76986298+Ankichaw@users.noreply.github.com>
2021-02-28 19:54:16 +0530

    Add files via upload

---
+++ sql/sqlscript (3).sql
@@ -0,0 +1,10 @@
+REM   Script: Activity5
+REM   Activity5
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
